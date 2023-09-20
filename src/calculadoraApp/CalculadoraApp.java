a201a9850342b6887b81e3f1997976514acb8bb1
Elias Oliveira <eliassousaoliveira@hotmail.com>
2023-09-20 08:45:40 -0300

    Refatorando código para melhor legibilidade

--- src/calculadoraApp/CalculadoraApp.java
+++ src/calculadoraApp/CalculadoraApp.java
@@ -6,57 +6,67 @@ public class CalculadoraApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		Double num1;
-		Double num2;
-		String operacao;
 		boolean continuar;
-		
+
 		do {
-			System.out.println("\nDigite o primeiro valor:");
-			num1 = scanner.nextDouble();
-			
-			System.out.println("\nDigite a operação ( +, -, /, *): ");
-			operacao = scanner.next();
-			
-			System.out.println("\nDigite o segundo valor:");
-			num2 = scanner.nextDouble();
-			
-			System.out.println("Resultado: " + realizarCalculo(num1, num2, operacao));
-			
+			double num1 = lerNumero("Digite o primeiro valor: ");
+			String operacao = lerOperacao("Digite a operação (+, -, /, *): ");
+			double num2 = lerNumero("Digite o segundo valor: ");
+
+			if (operacaoValida(operacao)) {
+				double resultado = realizarCalculo(num1, num2, operacao);
+				System.out.println("Resultado: " + resultado);
+			} else {
+				System.out.println("Operação Inválida");
+			}
+
 			continuar = verificarNovaOperacao();
-			
-		}while ( continuar);
+		} while (continuar);
 		
-	}
-	
-	public static boolean verificarNovaOperacao() {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Deseja continuar uma nova operação? ( S ou N): ");
-		return scan.nextLine().toUpperCase().equals("S");
+		System.out.println("Obrigado por usar a calculadora!");
+
 	}
 
-	public static Double realizarCalculo(Double num1, Double num2, String operacao) {
-		Double respostaCalculo = 0.0;
+	private static double lerNumero(String mensagem) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(mensagem);
+		return scanner.nextDouble();
+	}
 
+	private static String lerOperacao(String mensagem) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(mensagem);
+		return scanner.next();
+	}
+
+	private static boolean operacaoValida(String operacao) {
+		return "+".equals(operacao) || "-".equals(operacao) || "*".equals(operacao) || "/".equals(operacao);
+	}
+
+	private static double realizarCalculo(double num1, double num2, String operacao) {
 		switch (operacao) {
 		case "+":
-			respostaCalculo = num1 + num2;
-			break;
+			return num1 + num2;
 		case "-":
-			respostaCalculo = num1 - num2;
-			break;
-		case "/":
-			respostaCalculo = num1 / num2;
-			break;
+			return num1 - num2;
 		case "*":
-			respostaCalculo = num1 * num2;
-			break;
-
+			return num1 * num2;
+		case "/":
+			if (num2 != 0) {
+				return num1 / num2;
+			} else {
+				System.out.println("Divisão por zero não é permitida");
+				return Double.NaN;
+			}
 		default:
-			System.out.println("Operação inválida");
+			return Double.NaN;
 		}
-		return respostaCalculo;
+	}
+
+	private static boolean verificarNovaOperacao() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Deseja continuar uma nova operação? (S ou N): ");
+		return "S".equalsIgnoreCase(scanner.next());
 	}
 
 }
